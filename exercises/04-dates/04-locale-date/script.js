66aae0124b414c37d5c71f531568a73d1ecc902d
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-03 18:11:50 +0100

    locale date exercise

--- exercises/04-dates/04-locale-date/script.js
+++ exercises/04-dates/04-locale-date/script.js
@@ -12,4 +12,10 @@
 (() => {
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
     // your code here
+    const mois= ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"]
+    const jours =["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samdi", "Dimanche"]
+    const d = new Date();
+    
+
+    document.getElementById("target").innerHTML = `${jours[d.getDay()]} ${d.getDate()} ${mois[d.getMonth()]}, ${d.getHours()}h${d.getMinutes(2)}`
 })();
