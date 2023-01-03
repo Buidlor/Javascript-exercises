510186e21b867aa86d2bf90c52792c8b7dcfc99f
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-03 17:41:09 +0100

    4-dates up to age by select exercise

--- exercises/04-dates/01-title-by-hour-one/script.js
+++ exercises/04-dates/01-title-by-hour-one/script.js
@@ -12,4 +12,8 @@
 (() => {
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
     // your code here
+    const d = new Date();
+    let time = d.getHours()*3600 + d.getMinutes()*60 + d.getSeconds();
+     
+    document.getElementById("target").innerHTML = `${time >= 3600*18 ? "Bonsoir" : "Bonjour"}`
 })();
--- exercises/04-dates/03-age-by-select/script.js
+++ exercises/04-dates/03-age-by-select/script.js
@@ -11,4 +11,34 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        const d = new Date();
+        let jour= document.getElementById("dob-day").value
+        let mois= document.getElementById("dob-month").value
+        let annee= document.getElementById("dob-year").value
+        let age = 0
+       
+        if ( d.getMonth()+1 > mois){
+            age = d.getFullYear() - annee 
+            console.log(" bd past months")
+        } 
+        else if(d.getMonth()+1 ==  mois){
+            if(d.getDate() >= jour ){ 
+                age = d.getFullYear() - annee
+                console.log("bd past few days")
+            } 
+            else {
+                age = d.getFullYear() - annee -1
+                console.log("bd this month but not yet happened")
+            }
+        }
+        else{
+            age = d.getFullYear() - annee -1
+            console.log("bd in months")
+        } 
+       alert(`your age is ${age}`)
+        
+    });
+    
+
 })();
