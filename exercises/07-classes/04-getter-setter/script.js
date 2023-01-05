40c7eaf3f9656c6511332905b40c25a6d3df42e4
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-05 12:43:28 +0100

    classes: setter and getter

--- exercises/07-classes/04-getter-setter/script.js
+++ exercises/07-classes/04-getter-setter/script.js
@@ -11,4 +11,23 @@
 
 (() => {
     // your code here
+    class Person{
+        constructor(firstname, lastname){
+          this.firstname = firstname
+          this.lastname =lastname  
+        }
+        get name(){
+            return this.setName()
+        }
+
+        setName(){
+            return `${this.firstname} ${this.lastname}`
+        }
+    }
+    document.getElementById("run").addEventListener("click", () => {
+        let people = new Person('Henry', 'Sehudo')
+        console.log(people.setName())
+        people = new Person('Carl', 'Smith') 
+        console.log(people.setName())
+    })
 })();
