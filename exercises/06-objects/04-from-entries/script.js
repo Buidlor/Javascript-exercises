4a80a5eb0b4f9276013275ef85ecfbfb40472d39
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-05 09:28:38 +0100

    objects fromEntries

--- exercises/06-objects/04-from-entries/script.js
+++ exercises/06-objects/04-from-entries/script.js
@@ -13,4 +13,10 @@
     const keys = ["name", "species", "age", "gender", "color"];
     const values = ["Skitty", "cat", 9, "female", "tabby"];
     // your code here
+    
+    document.getElementById("run").addEventListener("click", () => {
+        const array= []
+        keys.forEach((ele,i) => array.push([ele, values[i]]))
+        console.log(Object.fromEntries(array))
+    })
 })();
