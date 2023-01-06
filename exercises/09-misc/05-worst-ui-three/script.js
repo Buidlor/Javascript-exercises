5031daf57f0c39d30ac3b1a8c3bb4ca44bc4b9c7
Buidlor <jefboone@gmail.com>
2023-01-06 16:45:31 +0100

    interface buttons etc

--- exercises/09-misc/05-worst-ui-three/script.js
+++ exercises/09-misc/05-worst-ui-three/script.js
@@ -11,4 +11,34 @@
 
 (() => {
     // your code here
+
+    const input = [
+        document.getElementById("part-one"), 
+        document.getElementById("part-two"), 
+        document.getElementById("part-three"),
+        document.getElementById("part-four")
+    ]
+    const button = [
+        document.getElementById("fix-part-one"), 
+        document.getElementById("fix-part-two"), 
+        document.getElementById("fix-part-three"),
+        document.getElementById("fix-part-four")
+    ]
+    const Interval1 = setInterval(() => {
+        console.log("my friend")
+        input.map(ele => ele.value = Math.floor(Math.random() * (499 - 460 + 1)) + 460 )
+    }, 700); 
+    
+        input[1].value = Math.floor(Math.random()*100)
+        input[2].value = Math.floor(Math.random()*100) 
+        input[3].value = Math.floor(Math.random()*100)
+
+
+        button.map((ele,i) => ele.addEventListener("click", () => {
+    
+        } ))
+
+
+  
+    
 })();
