b77427d2cdb0bb78932775bbdbb69ef22d3fd7c6
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-08 23:08:14 +0100

    add to api

--- exercises/11-fetch/04-add/script.js
+++ exercises/11-fetch/04-add/script.js
@@ -11,4 +11,31 @@
 
 (() => {
     // your code here
+    document.getElementById('run').addEventListener('click', () =>{
+        const nom = document.getElementById('hero-name').value
+        const alterEgo = document.getElementById('hero-alter-ego').value
+        const pouvoirs = document.getElementById('hero-powers').value
+       
+
+        if(nom && alterEgo && pouvoirs){
+            console.log('you filled in text')
+            const addHero = {
+                name: nom,
+                alterEgo: alterEgo,
+                abilities: pouvoirs
+            }
+            fetch('http://localhost:3000/heroes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(addHero),
+            })
+            
+            console.log(addHero)
+        }
+        else{
+            alert("missing info, make sure to fill in the boxes")
+        }
+    })
 })();
