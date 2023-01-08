550d26fe8cc763e841e8eccd89b20b75b8cdc55e
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-08 18:07:50 +0100

    get id of the api adn insert the data bound to the ID in the  DOM

--- exercises/11-fetch/03-details/script.js
+++ exercises/11-fetch/03-details/script.js
@@ -11,4 +11,30 @@
 
 (() => {
     // your code here
+   
+
+    document.getElementById('run').addEventListener('click',() => {
+        
+        const template = document.getElementById('tpl-hero')
+        const target = document.getElementById('target')
+
+        let herroId = Number(document.getElementById('hero-id').value)
+        let inputname = document.getElementsByClassName('name')
+        let inputAlterEgo= document.getElementsByClassName('alter-ego')
+        let inputPowers= document.getElementsByClassName('powers')
+
+        fetch('http://localhost:3000/heroes')
+        .then(response => response.json())
+        .then(data => {
+            
+            target.innerHTML = template.innerHTML
+            inputname[0].innerHTML = data[herroId-1].name
+            inputAlterEgo[0].innerHTML = data[herroId-1].alterEgo
+            inputPowers[0].innerHTML = data[herroId-1].abilities
+            
+        })
+        
+         
+        
+    })
 })();
